26f8e3ff94d2eacb3b6f4af886b61d71ccd269b4
23CABPREMKUMARP <139529932+23CABPREMKUMARP@users.noreply.github.com>
2024-12-17 14:00:16 +0000

    1

--- test.java
+++ test.java
@@ -1,20 +1,38 @@
 import java.util.Scanner;
 
-class Test {
+public class test {
     public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);
+                    
 
-    System.out.println("Enter name, age and salary:");
+    System.out.println("Enter name, age, and salary:");
+                                            
+    String name = myObj.next();    
+    int age = myObj.nextInt();     
+    double salary = myObj.nextDouble(); 
+                                                                            
+                                                                              
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);                                                
+    System.out.println("Salary: " + salary);
 
-    String name = myObj.nextLine();
-    int age = myObj.nextInt();
-     double salary = myObj.nextDouble();
 
-     System.out.println("Name: " + name);
-     System.out.println("Age: " + age);
-     System.out.println("Salary: " + salary);
-   }
-  }
+    }  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
